038a7593fba761dd4085fec9d48b2e892256fca7
k1r1m1 <kirimi@paygoenergy.co>
2020-11-29 09:48:23 +0300

    customize site for/to LizWeaveologist

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import Masonry from 'react-masonry-component'
 import Img from 'gatsby-image'
 import Layout from "../components/layout"
@@ -10,17 +10,7 @@ const IndexPage = ({ data }) => (
       {data.allDatoCmsWork.edges.map(({ node: work }) => (
         <div key={work.id} className="showcase__item">
           <figure className="card">
-            <Link to={`/works/${work.slug}`} className="card__image">
-              <Img fluid={work.coverImage.fluid} />
-            </Link>
-            <figcaption className="card__caption">
-              <h6 className="card__title">
-                <Link to={`/works/${work.slug}`}>{work.title}</Link>
-              </h6>
-              <div className="card__description">
-                <p>{work.excerpt}</p>
-              </div>
-            </figcaption>
+            <Img fluid={work.coverImage.fluid} />
           </figure>
         </div>
       ))}
@@ -38,7 +28,6 @@ export const query = graphql`
           id
           title
           slug
-          excerpt
           coverImage {
             fluid(maxWidth: 450, imgixParams: { fm: "jpg", auto: "compress" }) {
               ...GatsbyDatoCmsSizes
