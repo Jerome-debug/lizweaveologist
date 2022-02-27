8cfdaa0db3aef84a3502e21e958ca88bb1203623
K1r1m1 <kirimi@paygoenergy.co>
2022-02-27 17:09:28 +0300

    load some images dynamically

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,32 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import Header from "../components/header";
 import Feature from "../components/feature";
 import Gallery from "../components/gallery";
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
   return (
     <>
       <Header />
       <Feature />
-      <Gallery />
+      <Gallery imgData={data} />
     </>
   );
 };
 
 export default IndexPage;
+
+export const query = graphql`
+  query IndexPageQuery {
+    allImageSharp {
+      edges {
+        node {
+          fluid {
+            originalName
+          }
+          gatsbyImageData
+        }
+      }
+    }
+  }
+`;
