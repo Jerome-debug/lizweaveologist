e96bbe1068b2077dadc52f1615c197bae9576210
K1r1m1 <kirimi@paygoenergy.co>
2022-02-27 14:09:10 +0300

    configure gallery area - replace npm for yarn

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -24,55 +24,34 @@ export default function Gallery() {
           </svg>
         </div>
 
-        <div className="container mx-auto px-4 lg:pt-24 lg:pb-64">
+        <div className="container mx-auto px-4 lg:pt-24 lg:pb-10">
           <div className="flex flex-wrap justify-center text-center">
             <div className="w-full px-4 lg:w-6/12">
               <h2 className="text-4xl font-semibold text-white">My Work</h2>
-              {/*TODO add grid images here*/}
-              <p className="mt-4 mb-4 text-lg leading-relaxed text-gray-500">
-                Put the potentially record low maximum sea ice extent tihs year
-                down to low ice. According to the National Oceanic and
-                Atmospheric Administration, Ted, Scambos.
-              </p>
             </div>
           </div>
-          <div className="mt-12 flex flex-wrap justify-center">
-            <div className="w-full px-4 text-center lg:w-3/12">
-              <div className="inline-flex h-12 w-12 items-center justify-center rounded-full bg-white p-3 text-gray-900 shadow-lg">
-                <i className="fas fa-medal text-xl"></i>
-              </div>
-              <h6 className="mt-5 text-xl font-semibold text-white">
-                Excelent Services
-              </h6>
-              <p className="mt-2 mb-4 text-gray-500">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-            <div className="w-full px-4 text-center lg:w-3/12">
-              <div className="inline-flex h-12 w-12 items-center justify-center rounded-full bg-white p-3 text-gray-900 shadow-lg">
-                <i className="fas fa-poll text-xl"></i>
-              </div>
-              <h5 className="mt-5 text-xl font-semibold text-white">
-                Grow your market
-              </h5>
-              <p className="mt-2 mb-4 text-gray-500">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-            <div className="w-full px-4 text-center lg:w-3/12">
-              <div className="inline-flex h-12 w-12 items-center justify-center rounded-full bg-white p-3 text-gray-900 shadow-lg">
-                <i className="fas fa-lightbulb text-xl"></i>
-              </div>
-              <h5 className="mt-5 text-xl font-semibold text-white">
-                Launch time
-              </h5>
-              <p className="mt-2 mb-4 text-gray-500">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
+
+          {/* TODO xs:grid-cols-2 affects md:grid-cols-4 */}
+          {/* TODO add pulse animation */}
+          <div className="grid min-h-fit max-w-6xl place-items-center gap-y-4 px-4 py-4 mx-auto sm:grid-cols-2 md:grid-cols-4">
+            <p className="mt-4 mb-4 text-lg leading-relaxed text-gray-500 xs:col-span-2">
+              Put the potentially record low maximum sea ice extent tihs year
+              down to low ice. According to the National Oceanic and
+              Atmospheric Administration, Ted, Scambos.
+            </p>
+            {/*TODO add grid images here*/}
+            <div className="h-16 w-5/6 bg-red-500 xs:h-auto xs:square md:col-start-4"></div>
+            <div className="h-10 w-5/6 bg-blue-500 xs:h-auto xs:square md:col-start-2"></div>
+            <div className="h-10 w-5/6 bg-pink-500 xs:h-auto xs:square"></div>
+            <div className="h-10 w-5/6 bg-blue-500 xs:h-auto xs:square"></div>
+            <div className="h-10 w-5/6 bg-pink-500 xs:h-auto xs:square"></div>
+            <div className="h-10 w-5/6 bg-blue-500 xs:h-auto xs:square"></div>
+            <div className="h-10 w-5/6 bg-blue-500 xs:h-auto xs:square"></div>
+            <div className="h-10 w-5/6 bg-pink-500 xs:h-auto xs:square"></div>
+            <div className="h-10 w-5/6 bg-pink-500 xs:h-auto xs:square"></div>
+            <div className="h-10 w-5/6 bg-blue-500 xs:h-auto xs:square"></div>
+            <div className="h-10 w-5/6 bg-blue-500 xs:h-auto xs:square"></div>
+            <div className="h-10 w-5/6 bg-pink-500 xs:h-auto xs:square"></div>
           </div>
         </div>
       </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.{js, jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens:{
+        xs: '540px',
+        ...defaultTheme.screens,
+      },
       colors: {
         body: "#17171F",
       },
